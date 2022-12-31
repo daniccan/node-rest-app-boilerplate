108c403e7d92aa6de1eccdc574eaa79f340b633d
daniccan <passion.dani@gmail.com>
2022-12-31 15:58:35 +0530

    Add API endpoint for sync controller

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const syncController = require("../src/controller/sync_controller");
 
 router.get("/health", (req, res, next) => {
   res.send("OK");
 });
+
+router
+  .route("/sync")
+  .post(syncController.runSyncJob);
 
 module.exports = router;
